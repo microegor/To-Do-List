9d158e5619c8d533962b90d6c22e860999a63993
microegor <microegor321@gmail.com>
2024-09-25 17:11:06 +0200

    feat: Добавлен класс InputDialog и необходимые using директивы

---
+++ Window1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToDoList
+{
+    public partial class InputDialog : Window
+    {
+        public string InputText { get; private set; }
+
+        public InputDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputText = InputTextBox.Text;
+            DialogResult = true;
+        }
+    }
+}
